4964872a32ec36bdeb0e2bffe3f4ee6d46f22840
Yeisson Lopez <yeissonllespinosa@hotmail.com>
2023-11-02 22:51:33 -0500

    Update

--- practicasAcademicas/api/applicartionPractice/services/CreatedApplicartion/NotificationInvitates.py
+++ practicasAcademicas/api/applicartionPractice/services/CreatedApplicartion/NotificationInvitates.py
@@ -13,8 +13,6 @@ class NotificationInvitatesEMAIL():
         """
         Estimado ##FuncionUsuario##
         ###Nombre de Usuario###
-
-        
         Ha recibido una solicitud de asistencia para la practica ### Nombre de la Practica ### de la Materia ###Nombre de la Asignatura### dirijida por el docente
         ###Nombre del Docente### Por favor acceda al sistema de informacion Chaira ###Ruta depende de la funcion de usuario### para que revise la solicitud y acepte o 
         la rechazar
